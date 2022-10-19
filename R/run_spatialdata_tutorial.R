9d3c27a0afb2da7de9fe0b537b35fe64dceb8f79
Eli Pousson <eli.pousson@gmail.com>
2022-10-19 11:20:36 -0400

    feat: add available_spatialdata_tutorials
    - feat: add available_spatialdata_tutorials
    - refactor: drop use of system data to validate tutorial names
    - refactor: import gradethis

--- R/run_spatialdata_tutorial.R
+++ R/run_spatialdata_tutorial.R
@@ -1,10 +1,14 @@
 #' Run a Building Spatial Data interactive tutorial
 #'
-#' A wrapper for [learnr::run_tutorial()] with validation for the tutorial name.
+#' Use [run_spatialdata_tutorial()] to run a tutorial or
+#' [available_spatialdata_tutorials()] to list available tutorials in this
+#' package. These function are minimal wrappers for [learnr::run_tutorial()] and
+#' [learnr::available_tutorials()].
 #'
-#' @param name Tutorial name.
-#' @param ... Additional parameters passed to [learnr::run_tutorial()]
-#' @return Launch a learnr exercise.
+#' @inheritParams learnr::run_tutorial
+#' @param ... Additional parameters passed to [learnr::run_tutorial()]. Not used
+#'   for [available_spatialdata_tutorials()].
+#' @return Launch a learnr tutorial or list available tutorials.
 #' @examples
 #' \dontrun{
 #' if(interactive()){
@@ -17,10 +21,17 @@
 #' @export
 #' @importFrom learnr run_tutorial
 run_spatialdata_tutorial <- function(name, ...) {
-  name <- match.arg(tolower(name), tutorial_names)
   learnr::run_tutorial(
     name = name,
     package = "spatialdatalearnr",
     ...
     )
+}
+
+#' @name available_spatialdata_tutorials
+#' @rdname run_spatialdata_tutorial
+#' @export
+#' @importFrom learnr available_tutorials
+available_spatialdata_tutorials <- function(...) {
+  learnr::available_tutorials(package = "spatialdatalearnr")
 }
